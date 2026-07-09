fe25c0cebb3ee5dac89e75ea83d480c6231493c0
Priyanshi Goyal <p.25012008.g@gmail.com>
2026-07-09 12:21:44 +0530

    done leetcode problem number 1431

--- 1431.cpp
+++ 1431.cpp
@@ -1,16 +1,27 @@
-class Solution {
-public:
-    vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+#include <bits/stdc++.h>
+using namespace std;
+vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
 
-        int maxCandies = *max_element(candies.begin(), candies.end());
+    int maxCandies = *max_element(candies.begin(), candies.end());
 
-        vector<bool> ans;
+    vector<bool> ans;
 
-        for(int i = 0; i < candies.size(); i++)
-        {
-            ans.push_back(candies[i] + extraCandies >= maxCandies);
-        }
-
-        return ans;
+    for(int i = 0; i < candies.size(); i++){
+        ans.push_back(candies[i] + extraCandies >= maxCandies);
     }
-};
+
+    return ans;
+}
+int main() {
+    vector<int> candies = {2, 3, 5, 1, 3};
+    int extraCandies = 3;
+
+    vector<bool> result = kidsWithCandies(candies, extraCandies);
+
+    for (bool hasMax : result) {
+        cout << (hasMax ? "true" : "false") << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
